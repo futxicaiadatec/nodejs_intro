34c68bedd1fad59f901b39ee24ec77a203df1043
itacir gabral <degumes@users.noreply.github.com>
2017-11-03 08:49:44 -0300

    02_02_transform fix names

--- 02_nodismo/02_stream/03_transform.js
+++ 02_nodismo/02_stream/03_transform.js
@@ -3,13 +3,12 @@ var entrada = process.stdin
 var saida = process.stdout
 
 var lengths = []
-let count = 0
-var transform = new Stream.Transform({objectMode: true})
+var Transform = new Stream.Transform({objectMode: true})
 
-transform._transform = function(chunk,enc,done){
+Transform._transform = function(chunk,enc,done){
   var data = chunk.toString()
   lengths.push(data.length)
-  var strlen = JSON.stringify(lengths,null,2)
+  var strlen = JSON.stringify(lengths,null,2)+"\n"
   done(null,strlen)
 }
 
